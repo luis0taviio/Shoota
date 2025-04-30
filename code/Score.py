edbf7a245f7b309ad9c81b6489846e62189898b4
Luis <luisotavio20novembro@gmail.com>
2025-04-30 20:19:11 -0300

    changing color score purple to black

--- code/Score.py
+++ code/Score.py
@@ -66,8 +66,8 @@ class Score:
         pygame.mixer_music.load('./asset/Score.mp3')
         pygame.mixer_music.play(-1)
         self.window.blit(source=self.surf, dest=self.rect)
-        self.score_text(48,'TOP 10 SHOOTA', C_PURPLE, SCORE_POS['Title'])
-        self.score_text(20, 'NAME       SCORE       DATE     ', C_PURPLE, SCORE_POS['Label'])
+        self.score_text(48,'TOP 10 SHOOTA', C_BLACK, SCORE_POS['Title'])
+        self.score_text(20, 'NAME      SCORE       DATE     ', C_BLACK, SCORE_POS['Label'])
         db_proxy = DBProxy('DBScore')
         list_score = db_proxy.retrieve_top10()
         db_proxy.close()
@@ -75,7 +75,7 @@ class Score:
 
         for player_score in list_score:
             id_, name, score, date = player_score
-            self.score_text(20,f'{name}     {score :05d}    {date}', C_PURPLE,
+            self.score_text(20,f'{name}     {score :05d}    {date}', C_BLACK,
                             SCORE_POS[list_score.index(player_score)])
 
 
